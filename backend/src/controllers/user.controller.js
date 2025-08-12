a33fa513f24d5f2286099499009135dabe0de0b6
Beauty Kumari <beautykumari29010@gmail.com>
2025-08-12 19:54:24 +0530

    final commit

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,62 +1,72 @@
 import httpStatus from "http-status";
-import {User} from "../models/user.models.js";
-import bcrypt,{hash} from "bcryptjs";
-import crypto from "crypto"
+import { User } from "../models/user.models.js";
+import bcrypt, { hash } from "bcrypt"
 
+import crypto from "crypto"
+import { Meeting } from "../models/meeting.models.js";
 const login = async (req, res) => {
+
     const { username, password } = req.body;
 
     if (!username || !password) {
-        return res.status(400).json({ message: "Please provide username and password" });
+        return res.status(400).json({ message: "Please Provide" })
     }
 
     try {
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" });
+            return res.status(httpStatus.NOT_FOUND).json({ message: "User Not Found" })
         }
 
-        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+        let isPasswordCorrect = await bcrypt.compare(password, user.password)
+
         if (isPasswordCorrect) {
             let token = crypto.randomBytes(20).toString("hex");
 
             user.token = token;
             await user.save();
-            return res.status(httpStatus.OK).json({ token: token });
+            return res.status(httpStatus.OK).json({ token: token })
         } else {
-            return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Credentials" });
+            return res.status(httpStatus.UNAUTHORIZED).json({ message: "Invalid Username or password" })
         }
+
     } catch (e) {
-        return res.status(500).json({ message: `Something went wrong ${e}` });
+        return res.status(500).json({ message: `Something went wrong ${e}` })
     }
 }
 
 
-const register=async(req,res)=>{
-    const {name,username,password}=req.body;
+const register = async (req, res) => {
+    const { name, username, password } = req.body;
 
-    try{
-        const existingUser=await User.findOne({username});
-        if(existingUser){
-            return res.status(httpStatus.FOUND).json({message:"User already exists"});
+
+    try {
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+            return res.status(httpStatus.FOUND).json({ message: "User already exists" });
         }
-        const hashedPassword=await bcrypt.hash(password,10);
 
-        const newUser=new User({
-            name:name,
-            username:username,
-            password:hashedPassword
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            name: name,
+            username: username,
+            password: hashedPassword
         });
+
         await newUser.save();
 
-        res.status(httpStatus.CREATED).json({message:"User Registered"})
+        res.status(httpStatus.CREATED).json({ message: "User Registered" })
 
-        }catch(e){
-           res.json({message:`something went wrong${e}`})
-        }
+    } catch (e) {
+        res.json({ message: `Something went wrong ${e}` })
     }
 
-   const getUserHistory = async (req, res) => {
+}
+
+
+const getUserHistory = async (req, res) => {
     const { token } = req.query;
 
     try {
