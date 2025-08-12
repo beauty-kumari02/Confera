389cd73ebed62edafd72ca163c4d8735663535d5
Beauty Kumari <beautykumari29010@gmail.com>
2025-08-12 20:05:23 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -1,44 +1,39 @@
-import express from"express";
+import express from "express";
+import { createServer } from "node:http";
+
+import { Server } from "socket.io";
+
 import mongoose from "mongoose";
+import { connectToSocket } from "./src/controllers/socketManager.js";
 
-import {createServer} from "node:http";
-
-import {Server} from "socket.io";
-
-import connectToSocket from "./src/controllers/socketManager.js";
 import cors from "cors";
-import userRoutes from "./src/routes/users.routes.js"
-import newUserRoutes from "./src/routes/users.routes.js"
+import userRoutes from "./src/routes/users.routes.js";
 
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
 
-const app=express();
-const server=createServer(app);
-const io=connectToSocket(server);
 
-app.set("port",(process.env.PORT || 8000))
+app.set("port", (process.env.PORT || 8000))
 app.use(cors());
-app.use(express.json({limit:"40kb"}));//to limit the payload
-app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-app.use("/api/v1/users",userRoutes);
-app.use("/api/v2/users",newUserRoutes);
+app.use("/api/v1/users", userRoutes);
 
-
-const start =async ()=>{
+const start = async () => {
     app.set("mongo_user")
-      const connectionDb=await mongoose.connect("mongodb+srv://beautykumari02:Baty1610@conferacluster.jh4hvvi.mongodb.net/?retryWrites=true&w=majority&appName=ConferaCluster");
+    const connectionDb = await mongoose.connect("mongodb+srv://beautykumari02:Baty1610@conferacluster.jh4hvvi.mongodb.net/?retryWrites=true&w=majority&appName=ConferaCluster")
 
-      console.log(`MONGO Connected DB Host:${connectionDb.connection.host}`)
-      server.listen(app.get("port"),()=>{
-        console.log("LISTENING ON PORT 8000");
-      });
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
+    server.listen(app.get("port"), () => {
+        console.log("LISTENIN ON PORT 8000")
+    });
+
 
 
 }
+
 
 
 start();
